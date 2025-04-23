8c69722ea7244a6f5fe3e11e381715535f2641f0
Gabriel Patara <gabriel.patara@gmail.com>
2025-04-22 21:23:14 -0300

    frontend finalizado

--- src/app/fluxo-comercial/fluxo-comercial.component.ts
+++ src/app/fluxo-comercial/fluxo-comercial.component.ts
@@ -3,11 +3,19 @@ import { MatTableModule } from '@angular/material/table';
 import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-fluxo-comercial',
   standalone: true,
-  imports: [MatTableModule, CommonModule, MatToolbarModule],
+  imports: [
+    MatTableModule, 
+    CommonModule, 
+    MatToolbarModule,
+    MatSnackBarModule,
+    MatButtonModule
+  ],
   templateUrl: './fluxo-comercial.component.html',
   styleUrls: ['./fluxo-comercial.component.scss'],
 })
@@ -15,16 +23,59 @@ export class FluxoComercialComponent implements OnInit {
   displayedColumns: string[] = ['id', 'title', 'formatted_value', 'won_time'];
   public dataSource = [];
 
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private apiService: ApiService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
     this.apiService.refreshDados().subscribe({
-      next: (data: any) => {
-        this.dataSource = data;
+      next: (res: any) => {
+        console.log(res);
+  
+        this.dataSource = Array.isArray(res.data) ? res.data : [];
+  ;
       },
       error: (err: any) => {
-        console.error('Erro ao buscar dados:', err);
-      },
+        console.error('Erro ao buscar dados do servidor:', err);
+        this.snackBar.open(
+          'Não foi possível conectar ao servidor, verifique e tente novamente.',
+          'Fechar',
+          {
+            duration: 5000,
+            panelClass: ['snackbar-erro']
+          }
+        );
+      }
     });
   }
+
+  atualizarDados(){
+    this.apiService.refreshDados().subscribe({
+      next: (res: any) => {
+        this.dataSource = Array.isArray(res.data) ? res.data : [];
+        this.snackBar.open(
+          res.message,
+          'Fechar',
+          {
+            duration: 5000,
+            panelClass: ['snackbar-erro']
+          }
+        );
+  ;
+      },
+      error: (err: any) => {
+        console.error('Erro ao buscar dados do servidor:', err);
+        this.snackBar.open(
+          'Não foi possível conectar ao servidor, verifique e tente novamente.',
+          'Fechar',
+          {
+            duration: 5000,
+            panelClass: ['snackbar-erro']
+          }
+        );
+      }
+    });
+  };
+  
 }
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +11,11 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   refreshDados(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.FrontendUrl}/refresh`);
+    return this.http.get<any[]>(`${this.FrontendUrl}/refresh`).pipe(
+      catchError((err) => {
+        return throwError(() => err);
+      })
+    );
   }
+
 }
