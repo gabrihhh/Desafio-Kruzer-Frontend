f42cd299d58c821de3637a2021de70bc7fd7a8d0
Gabriel Patara <gabriel.patara@gmail.com>
2025-04-23 18:24:19 -0300

    front pronto

--- src/app/fluxo-comercial/fluxo-comercial.component.ts
+++ src/app/fluxo-comercial/fluxo-comercial.component.ts
@@ -1,34 +1,40 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-fluxo-comercial',
   standalone: true,
   imports: [
-    MatTableModule, 
-    CommonModule, 
+    MatTableModule,
+    CommonModule,
     MatToolbarModule,
     MatSnackBarModule,
     MatButtonModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatProgressSpinnerModule,
+    DatePipe
   ],
   templateUrl: './fluxo-comercial.component.html',
   styleUrls: ['./fluxo-comercial.component.scss'],
 })
 export class FluxoComercialComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['id', 'title', 'formatted_value', 'won_time'];
+  displayedColumns: string[] = ['person_name', 'org_name', 'formatted_value', 'won_time'];
   public dataSource = new MatTableDataSource<any>([]);
+  public loading: boolean = true;
 
+  // Configurações do paginator
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     private apiService: ApiService,
@@ -41,52 +47,70 @@ export class FluxoComercialComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+    this.dataSource.sortingDataAccessor = (item, property) => {
+      switch (property) {
+        case 'won_time': 
+          return new Date(item.won_time).getTime();
+        default: 
+          return item[property];
+      }
+    };
   }
 
   carregarDados() {
+    this.loading = true;
     this.apiService.refreshDados().subscribe({
       next: (res: any) => {
         const dados = Array.isArray(res.data) ? res.data : [];
         this.dataSource.data = dados;
+        
+        setTimeout(() => {
+          if (this.dataSource.paginator) {
+            this.dataSource.paginator._changePageSize(this.dataSource.paginator.pageSize);
+          }
+        });
+        
+        this.loading = false;
       },
       error: (err: any) => {
-        console.error('Erro ao buscar dados do servidor:', err);
-        this.snackBar.open(
-          'Não foi possível conectar ao servidor, verifique e tente novamente.',
-          'Fechar',
-          {
-            duration: 5000,
-            panelClass: ['snackbar-erro']
-          }
-        );
+        console.error('Erro ao buscar dados:', err);
+        this.mostrarErro('Erro ao carregar dados');
+        this.loading = false;
       }
     });
   }
 
   atualizarDados() {
+    this.loading = true;
     this.apiService.refreshDados().subscribe({
       next: (res: any) => {
-        this.dataSource.data = Array.isArray(res.data) ? res.data : [];
-        this.snackBar.open(
-          res.message,
-          'Fechar',
-          {
-            duration: 5000,
-            panelClass: ['snackbar-erro']
+        const dados = Array.isArray(res.data) ? res.data : [];
+        this.dataSource.data = dados;
+        
+        setTimeout(() => {
+          if (this.dataSource.paginator) {
+            this.dataSource.paginator.firstPage();
+            this.dataSource.paginator._changePageSize(this.dataSource.paginator.pageSize);
           }
-        );
+        });
+        
+        this.snackBar.open('Dados atualizados!', 'Fechar', { duration: 3000 });
+        this.loading = false;
       },
-      error: (err: any) => {
-        console.error('Erro ao buscar dados do servidor:', err);
-        this.snackBar.open(
-          'Não foi possível conectar ao servidor, verifique e tente novamente.',
-          'Fechar',
-          {
-            duration: 5000,
-            panelClass: ['snackbar-erro']
-          }
-        );
+      error: (err) => {
+        console.error('Erro ao atualizar:', err);
+        this.mostrarErro('Erro ao atualizar dados');
+        this.loading = false;
       }
+    });
+  }
+
+  private mostrarErro(mensagem: string) {
+    this.snackBar.open(mensagem, 'Fechar', {
+      duration: 5000,
+      panelClass: ['snackbar-error']
     });
   }
 }
